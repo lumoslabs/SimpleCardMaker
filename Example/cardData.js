a378f4524ec005d4bde10b5a4bedb6612556a41b
Eli <eli@lumoslabs.com>
2016-06-30 11:17:58 -0700

    Initial commit.

---
+++ Example/cardData.js
@@ -0,0 +1,80 @@
+/*global document*/
+
+//
+var getTypeACards = function()
+{
+    var typeACards =
+    [
+        "this",
+        "is",
+        "a",
+        "test",
+        "card"
+    ];
+    
+    var cardIndex;
+    for ( cardIndex = 0; cardIndex < typeACards.length; cardIndex++ )
+    {
+        typeACards[ cardIndex ] = "Appending prefixes: " + typeACards[ cardIndex ];
+    }
+    
+    return typeACards;
+};
+
+var getTypeBCards = function()
+{
+    var typeBTypes =
+    [
+        { title: "DUPED CARD",      count: 3,  text: "This card will be added 3 times. Look, HTML!<br/><br/>I did it." },
+        { title: "DUPED CARD 2",    count: 4,  text: "This card will be added 4 times." },
+        { title: "DUPED CARD 3",    count: 5,  text: "<b>5 times</b> holy crapola." }
+    ];
+    var typeBCards = [];
+
+    var typeIndex;
+    for ( typeIndex = 0; typeIndex < typeBTypes.length; typeIndex++ )
+    {
+        var type = typeBTypes[ typeIndex ];
+        var number;
+        for ( number = 0; number < type.count; number++ )
+        {
+            typeBCards.push( '<div class="customClass">' + type.title + '</div><br/>' + type.text );
+        }
+    }
+    
+    return typeBCards;
+};
+
+//all you need to do in cardData.js is create a document.cardData array that has
+//objects with a "title" string and "cards" array. Optionally, you can include
+//a "heading" which is the HTML heading tag to use for the text.
+//You can then use cardStyles.css to create different styles per title.
+//"cards" is an array of strings that will be the main body of the cards.
+document.cardData =
+[
+    {
+        title: "TYPE A",
+        cards: getTypeACards()
+    },
+    
+    {
+        title: "TYPE B",
+        heading: "h2",
+        cards: getTypeBCards()
+    },
+    
+    {
+        title: "TYPE C",
+        heading: "h3",
+        cards:
+        [
+            "Look",
+            "I",
+            "Made",
+            "Cards",
+            "Without",
+            "A",
+            "Function"
+        ]
+    }
+];
---
+++ simpleLayout.js
@@ -0,0 +1,53 @@
+/*global document*/
+/*global window*/
+
+var getCardHTML = function( titleStyle, titleText, cardText, heading )
+{
+    titleText = titleText || "CARD";
+    heading = heading || "h1";
+
+    var htmlBlock =
+    [
+        '',
+        '<div class="image">',
+        '    <img src="cardBacking.png" alt="" />',
+        '    <div class="floatingText">',
+        '        <h1 class="' + titleStyle + '">' + titleText + '</h1>',
+        '        <' + heading + '>' + cardText + '</' + heading + '>',
+        '    </div>',
+        '</div>',
+        ''
+    ].join( '\n' );
+    
+    return htmlBlock;
+};
+
+//build the cards once the window loads
+window.onload = function()
+{
+    var cardHTML = "";  
+    
+    if ( !document.cardData )
+    {
+        cardHTML = "NO CARD DATA. Make sure to define document.cardData in cardData.js. It should be an array of objects, where each object has the schema {title:\"\", heading:\"h1\", cards:[]}.";
+    }
+    else
+    {
+        var cardTypeIndex;
+        var cardIndex;
+        for ( cardTypeIndex = 0; cardTypeIndex < document.cardData.length; cardTypeIndex++ )
+        {
+            var cardTypeData = document.cardData[ cardTypeIndex ];
+        
+            if ( cardTypeData && cardTypeData.cards )
+            {
+                for ( cardIndex = 0; cardIndex < cardTypeData.cards.length; cardIndex++ )
+                {
+                    cardHTML += getCardHTML( "type" + cardTypeIndex + "Title", cardTypeData.title, cardTypeData.cards[ cardIndex ], cardTypeData.heading );
+                }
+            }
+        }
+    }
+    
+    document.getElementById("cards").innerHTML = cardHTML;
+};
